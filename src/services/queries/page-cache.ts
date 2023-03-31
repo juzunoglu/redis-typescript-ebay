5dc8898c201bdde626e0e6a8ad03c8a5a22d3eb2
alihan.uzunoglu <alihan.uzunoglu@jollifygames.com>
2023-03-31 12:05:35 +0300

    cache static pages

--- src/services/queries/page-cache.ts
+++ src/services/queries/page-cache.ts
@@ -1,3 +1,19 @@
-export const getCachedPage = (route: string) => {};
+import { client } from "$services/redis";
+import { pageCacheKey } from "$services/keys";
+const cacheRoutes = ['/about', '/privacy', '/auth/signin', '/auth/signup']
 
-export const setCachedPage = (route: string, page: string) => {};
+export const getCachedPage = (route: string) => {
+    if(cacheRoutes.includes(route)) {
+        return client.get(pageCacheKey(route));
+    }
+
+    return null;
+};
+
+export const setCachedPage = (route: string, page: string) => {
+    if(cacheRoutes.includes(route)) {
+        client.set(pageCacheKey(route), page, {
+            EX: 2
+        });
+    }
+};
